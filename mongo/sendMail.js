8418f3f5d02f76b845c67bbcacea54d0b4c840dc
ArunkumarVK <vkarunkumar446@gmail.com>
2023-06-26 11:16:59 +0530

    first commit

---
+++ mongo/sendMail.js
@@ -0,0 +1,28 @@
+import nodemailer from "nodemailer";
+
+export const sendMail = async (email, subject, text) => {
+  const transporter = nodemailer.createTransport({
+    host: "smtppro.gmail.in",
+    port: 465,
+    secure: true, //ssl
+    auth: {
+      user: process.env.RESET_MAILID,
+      pass: process.env.RESET_MAILPASS,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  var mailOptions = {
+    from: process.env.RESET_MAILID,
+    to: email,
+    subject: subject,
+    text: text,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) console.log("Error in sending mail", error);
+    else console.log("Email sent:" + info.response);
+  });
+};
